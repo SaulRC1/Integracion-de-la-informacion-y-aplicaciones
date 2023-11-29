2f0534b10a85c68e4615f318b4517f963eaf5399
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2023-11-29 02:16:24 +0100

    AggregatorTask implementado

--- Practicas/Practica 1 - Implementacion CAFE/src/main/java/cafe/models/task/transforming/AggregatorTask.java
+++ Practicas/Practica 1 - Implementacion CAFE/src/main/java/cafe/models/task/transforming/AggregatorTask.java
@@ -1,12 +1,16 @@
 package cafe.models.task.transforming;
 
 import cafe.models.message.Message;
+import cafe.models.message.MessageBuilder;
 import cafe.models.task.Task;
 import cafe.models.slot.Slot;
 import cafe.xml.XPathParser;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,6 +18,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  *
@@ -64,13 +69,26 @@ public class AggregatorTask extends Task
         {
             System.out.println("SplitterId from Aggregator: " + allSplittersIterator.next());
         }
+        
+        Map<Long, List<Message>> messagesFromSplitterMap = createMessagesFromSplitterMap(allSplitters);
 
-        /*Message msg;
+        Message msg;
         
         while((msg = inputSlot.read()) != null)
         {
+            long splitterId = getSplitterId(msg);
             
-        }*/
+            List<Message> splitterIdMessageList = messagesFromSplitterMap.get(splitterId);
+            
+            splitterIdMessageList.add(msg);
+        }
+        
+        List<Message> aggregatedMessages = aggregateMessages(messagesFromSplitterMap);
+        
+        for (Message aggregatedMessage : aggregatedMessages)
+        {
+            outputSlot.write(aggregatedMessage);
+        }
     }
 
     /**
@@ -140,5 +158,79 @@ public class AggregatorTask extends Task
         }
         
         return -1;
+    }
+    
+    /**
+     * Will create a map with an empty list of messages for every splitter.
+     * 
+     * @param splittersIds The different splitters ids.
+     * @return A map containing an empty list of messages for every splitter.
+     */
+    private Map<Long, List<Message>> createMessagesFromSplitterMap(Set<Long> splittersIds) 
+    {
+        Map<Long, List<Message>> messagesFromSplitterMap = new HashMap<>();
+        
+        Iterator<Long> splittersIdsIterator = splittersIds.iterator();
+        
+        while(splittersIdsIterator.hasNext())
+        {
+            long splitterId = splittersIdsIterator.next();
+            
+            messagesFromSplitterMap.put(splitterId, new ArrayList<>());
+        }
+        
+        return messagesFromSplitterMap;
+    }
+
+    /**
+     * Aggregates all the messages present in the list of every splitter, in a
+     * single message.
+     * 
+     * @param messagesFromSplitterMap The {@link Map} that contains the list of
+     * messages for every splitter.
+     * 
+     * @return A {@link List} containing aggregated messages for every splitter
+     * present in the map.
+     */
+    private List<Message> aggregateMessages(Map<Long, List<Message>> messagesFromSplitterMap)
+    {
+        List<Message> aggregatedMessages = new ArrayList<>();
+        
+        MessageBuilder messageBuilder = new MessageBuilder();
+        
+        Set<Long> keySet = messagesFromSplitterMap.keySet();
+        
+        Iterator<Long> keySetIterator = keySet.iterator();
+        
+        while(keySetIterator.hasNext())
+        {
+            long key = keySetIterator.next();
+            
+            List<Message> messages = messagesFromSplitterMap.get(key);
+            
+            Message aggregatedMessage = messageBuilder.buildEmptyMessage();
+            
+            Document aggregatedMessageBody = aggregatedMessage.getDocument();
+            
+            for (Message message : messages)
+            {
+                Document messageBody = message.getDocument();
+                
+                NodeList messageBodyChildNodes = messageBody.getDocumentElement()
+                        .getChildNodes();
+                
+                for (int i = 0; i < messageBodyChildNodes.getLength(); i++)
+                {
+                    Node childNode = messageBodyChildNodes.item(i);
+                    
+                    Node importedChildNode = aggregatedMessageBody.importNode(childNode, true);
+                    aggregatedMessageBody.appendChild(importedChildNode);
+                }
+            }
+            
+            aggregatedMessages.add(aggregatedMessage);
+        }
+        
+        return aggregatedMessages;
     }
 }
