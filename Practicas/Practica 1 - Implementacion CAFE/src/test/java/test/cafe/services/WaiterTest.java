a11dc3f89b57a66b8363a31fd5b7e37a8d64d93b
SaulRC1 <saul.rodriguez.naranjo@gmail.com>
2023-12-05 22:09:35 +0100

    Testeo de Waiter

--- Practicas/Practica 1 - Implementacion CAFE/src/test/java/test/cafe/services/WaiterTest.java
+++ Practicas/Practica 1 - Implementacion CAFE/src/test/java/test/cafe/services/WaiterTest.java
@@ -1,8 +1,18 @@
 package test.cafe.services;
 
+import cafe.models.message.Message;
 import cafe.models.port.ExitPort;
 import cafe.models.slot.Slot;
+import cafe.services.Waiter;
+import cafe.xml.WrongDocumentExtensionException;
+import cafe.xml.XMLDocumentParser;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -13,9 +23,30 @@ public class WaiterTest
     @Test
     public void test()
     {
-        String destinationFilePath = "C:\\Users\\SaulRC1\\Documents\\4º Ingeniería Informática\\IIA";
-        
-        Slot exitPortSlot = new Slot();
-        ExitPort exitPort = new ExitPort(exitPortSlot);
+        try
+        {
+            String destinationFilePath = "C:\\Users\\SaulRC1\\Documents\\4º Ingeniería Informática\\IIA\\waiter_order";
+            
+            Slot exitPortSlot = new Slot();
+            ExitPort exitPort = new ExitPort(exitPortSlot);
+            
+            Waiter waiter = new Waiter(exitPort, destinationFilePath);
+            
+            Document messageBody = XMLDocumentParser.parseXMLDocument(
+                    "src/main/resources/WaiterTest/incoming_order_body.xml");
+            
+            Document messageMetadata = XMLDocumentParser.parseXMLDocument(
+                    "src/main/resources/WaiterTest/incoming_order_metadata.xml");
+            
+            Message msg = new Message(messageBody, messageMetadata);
+            
+            exitPort.write(msg);
+            
+            waiter.performFunctionality();
+            
+        } catch (ParserConfigurationException | SAXException | IOException | WrongDocumentExtensionException ex)
+        {
+            Logger.getLogger(WaiterTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
